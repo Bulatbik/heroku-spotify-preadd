41b4fee341dbec843a45da034633c4bdf75faf97
Bulatbik <bulatmegavolt@gmail.com>
2020-06-13 20:10:33 +0300

    test

--- server/index.js
+++ server/index.js
@@ -536,8 +536,8 @@ if (!isDev && cluster.isMaster) {
     // dynamicStatic.setPath(__dirname + '/public');
     // res.render(__dirname + '/public');
       console.log(jwtToken);
-    //res.sendFile(path.join(__dirname+'/public/index.html'), {data: jwtToken});
-      res.render(__dirname+ '/public/index.html', {jwtToken: jwtToken});
+      res.sendFile(path.join(__dirname+'/public/index.html'), {data: jwtToken});
+     // res.render(__dirname+ '/public/index.html', {jwtToken: jwtToken});
     //  res.render(path.join(__dirname + "/public/index.html"), {data: jwtToken});
   });
   app.get('/callback', function(req, res) {
