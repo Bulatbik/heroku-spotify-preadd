772f1749f002c42b9348986418e4fa68122fa71c
Bulatbik <bulatmegavolt@gmail.com>
2020-06-13 21:34:25 +0300

    hjk

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -155,12 +155,12 @@ class App extends Component {
                         <meta name="apple-music-app-name" content="My Cool Web App"/>
                             <meta name="apple-music-app-build" content="1978.4.1"/>
                     </head>
+                    <script src="https://js-cdn.music.apple.com/musickit/v1/musickit.js"></script>
+                    <a href='https://young-peak-41948.herokuapp.com/login' > PreAdd Album with Spotify </a>
+                    <a href='https://young-peak-41948.herokuapp.com/loginOne' > PreAdd Album with Apple Music </a>
+                    <button id="apple-music-authorize">apple-music-authorize</button>
+                    <button id="apple-music-unauthorize">apple-music-unauthorize</button>
                 </Helmet>
-                <script src="https://js-cdn.music.apple.com/musickit/v1/musickit.js"></script>
-                <a href='https://young-peak-41948.herokuapp.com/login' > PreAdd Album with Spotify </a>
-                <a href='https://young-peak-41948.herokuapp.com/loginOne' > PreAdd Album with Apple Music </a>
-                <button id="apple-music-authorize">apple-music-authorize</button>
-                <button id="apple-music-unauthorize">apple-music-unauthorize</button>
                 <div>
                     Now Playing: { this.state.nowPlaying.name }
                 </div>
