abfb0dff34bb51bcf02be89c37c811fa2b3408dd
Bulatbik <bulatmegavolt@gmail.com>
2020-06-15 17:40:57 +0300

    hjk

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -174,12 +174,21 @@ class App extends Component {
             }
         });
     }
+    signOut() {
+        let that = this;
+        co(function*() {
+            let status = yield that.musicInstance.unauthorize();
+            console.log(status);
+            that.setState({isLoginApple: false});
+        });
+    }
     render() {
         return (
             <div className="App">
                 <a href='https://young-peak-41948.herokuapp.com/login' > PreAdd Album with Spotify </a>
                 <a href='https://young-peak-41948.herokuapp.com/loginOne' > PreAdd Album with Apple Music </a>
                 <button onClick={() => this.signIn()} id="apple-music-authorize">apple-music-authorize</button>
+                <button onClick={() => this.signOut()}>apple-music-logout</button>
                 <div>
                     Now Playing: { this.state.nowPlaying.name }
                 </div>
