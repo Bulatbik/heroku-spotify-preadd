c33a206e7f54f37d7dc884543073f10ff5418d29
Bulatbik <bulatmegavolt@gmail.com>
2020-06-17 19:12:01 +0300

    ghj

--- server/index.js
+++ server/index.js
@@ -336,7 +336,7 @@ let rule = new schedule.RecurrenceRule();
 rule.tz = 'America/Chicago';
 // runs at 15:00:00
 rule.second = 0;
-rule.minute = 10;
+rule.minute = 14;
 rule.hour = 11;
 //import { v4 as uuidv4 } from 'uuid';
 const { v4: uuidv4 } = require('uuid');
@@ -492,7 +492,7 @@ async function scheduler() {
                 console.log(applepresaves.data[i].albumUPC+": ISRC code");
                 console.log(jwtToken+": jwtToken");
                console.log("option 3"+track.data.type);
-               console.dir(track.data)
+               console.dir(track.data[0])
 
             }catch(e){
                 uniqueNotReleasedAppleISRC.push(applepresaves.data[i].albumUPC);
