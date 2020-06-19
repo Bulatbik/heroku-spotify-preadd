d780b282c87540235a7d48f67837c24fb2bd15ce
Bulatbik <bulatmegavolt@gmail.com>
2020-06-19 17:16:37 +0300

    jk

--- server/index.js
+++ server/index.js
@@ -337,7 +337,7 @@ let rule = new schedule.RecurrenceRule();
 rule.tz = 'America/Chicago';
 // runs at 15:00:00
 rule.second = 0;
-rule.minute = 12;
+rule.minute = 19;
 rule.hour = 9;
 //import { v4 as uuidv4 } from 'uuid';
 const { v4: uuidv4 } = require('uuid');
@@ -553,8 +553,6 @@ async function scheduler() {
                 fetch(url, requestOptions)
                     .then(response => response.ok)
                     .then(result => console.log(result))
-                    .catch(error => console.log('error', error));
-
               /*  var options = {
                     'method': 'POST',
                     'url': 'https://api.music.apple.com/v1/me/library/?ids[albums]='+albumAppleID,
