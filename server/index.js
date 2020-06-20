263671cc2d8596ae6ad220ee261eb201ddae6667
Bulatbik <bulatmegavolt@gmail.com>
2020-06-20 10:30:57 +0300

    bnjmkl

--- server/index.js
+++ server/index.js
@@ -337,8 +337,8 @@ let rule = new schedule.RecurrenceRule();
 rule.tz = 'America/Chicago';
 // runs at 15:00:00
 rule.second = 0;
-rule.minute = 2;
-rule.hour = 16;
+rule.minute = 33;
+rule.hour = 2;
 //import { v4 as uuidv4 } from 'uuid';
 const { v4: uuidv4 } = require('uuid');
 
