bce7931be4ca34c1df0e3a7a8b26754d845a48b8
Adriel Lopez <adriel@endlessdigital.co>
2020-06-24 14:10:22 -0500

    fixed

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -193,7 +193,7 @@ class App extends Component {
             <div className="App">
                 <div className="bg-image"><img src="/Albumcover.png"/></div>
                 <div className="bg-tex-t">
-                    <img src="/Albumcover.png" style={{width: "100%", height: "10%", box-shadow: "5px solid grey"}}/>
+                    <img src="/Albumcover.png" style={{width: "100%", height: "10%"}}/>
                     <h1>Pre-Save/Pre-Add Light Path 8</h1>
                     <div>
                         <a className="buttonView" href='https://young-peak-41948.herokuapp.com/login'>
