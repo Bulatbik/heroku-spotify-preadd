d5e0017d7c9e81f246f1948b2c18f93b9a6d057a
Adriel Lopez <adriel@endlessdigital.co>
2020-06-25 17:39:49 -0500

    Changed checkbox setup

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -201,10 +201,10 @@ class App extends Component {
                             <img className="spotifyLogo" src="/Spotify_Logo_RGB_Green.png" />
                         <form  action="https://young-peak-41948.herokuapp.com/login" method="get">
                             <button className="button">Pre-Save on Spotify</button></form></a>
-                            <label class="artistupdates">
-                                <input type="checkbox" checked/> Get updates from KILLY
-                                <span class="checkmark"></span>
-                            </label>
+                            <form>
+                                <input type="checkbox" name=artistupdates value="artistupdates" checked>
+                                <label for="artistupdates"> Get updates from KILLY</label>
+                            </form>
                         <a className="buttonViewApple" href='https://young-peak-41948.herokuapp.com/login'>
                             <img className="appleLogo" src="/AppleMusic.png" />
                             <button className="buttonApple" onClick={() => this.signIn()} id="apple-music-authorize">Pre-Add on Apple Music</button>
