d622f635384b8d7d59563c2a81d1853ea38a5700
Adriel Lopez <adriel@endlessdigital.co>
2020-06-25 14:30:07 -0500

    spaced elements

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -201,9 +201,9 @@ class App extends Component {
                             <img className="spotifyLogo" src="/Spotify_Logo_RGB_Green.png" />
                         <form  action="https://young-peak-41948.herokuapp.com/login" method="get">
                             <button className="button">Pre-Save on Spotify</button></form></a>
-                            <form>
+                            <label>
                             <input type="checkbox" name="artistupdates" /> Get updates from KILLY
-                            </form>
+                            </label>
                         <a className="buttonViewApple" href='https://young-peak-41948.herokuapp.com/login'>
                             <img className="appleLogo" src="/AppleMusic.png" />
                             <button className="buttonApple" onClick={() => this.signIn()} id="apple-music-authorize">Pre-Add on Apple Music</button>
