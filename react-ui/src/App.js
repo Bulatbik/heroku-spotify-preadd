e0687c11752fada984fff2df8e0cef546132083b
Adriel Lopez <adriel@endlessdigital.co>
2020-06-25 19:14:46 -0500

    .

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -201,12 +201,10 @@ class App extends Component {
                             <img className="spotifyLogo" src="/Spotify_Logo_RGB_Green.png" />
                         <form  action="https://young-peak-41948.herokuapp.com/login" method="get">
                             <button className="button">Pre-Save on Spotify</button></form></a>
-                            <div class="artistupdates">
-                                <label>
-                                    <input type="checkbox" name="artistupdates"/>
-                                        <span>Get updates from KILLY</span>
+                                <label class="checkboxContainer">Get updates from KILLY
+                                    <input type="checkbox"/>
+                                    <span class="checkmark"></span>
                                 </label>
-                            </div>
                         <a className="buttonViewApple" href='https://young-peak-41948.herokuapp.com/login'>
                             <img className="appleLogo" src="/AppleMusic.png" />
                             <button className="buttonApple" onClick={() => this.signIn()} id="apple-music-authorize">Pre-Add on Apple Music</button>
