be8348ce3daf8d301fb91a03f823828d3607b976
Bulatbik <bulatmegavolt@gmail.com>
2020-06-30 13:11:04 +0300

    check

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,69 +6,6 @@ import SpotifyWebApi from "spotify-web-api-js";
 import {Helmet} from "react-helmet";
 const spotifyApi = new SpotifyWebApi();
 const axios = require('axios');
-/*function App() {
-  const [message, setMessage] = useState(null);
-  const [isFetching, setIsFetching] = useState(false);
-  const [url, setUrl] = useState('/api');
-  const fetchData = useCallback(() => {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        setMessage(json.message);
-        setIsFetching(false);
-      }).catch(e => {
-        setMessage(`API call failed: ${e}`);
-        setIsFetching(false);
-      })
-  }, [url]);
-
-  useEffect(() => {
-    setIsFetching(true);
-    fetchData();
-  }, [fetchData]);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        { process.env.NODE_ENV === 'production' ?
-            <p>
-              This is a production build from create-react-app.
-            </p>
-          : <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-        }
-        <p>{'« '}<strong>
-          {isFetching
-            ? 'Fetching message from API'
-            : message}
-        </strong>{' »'}</p>
-        <p><a
-          className="App-link"
-          href="https://github.com/mars/heroku-cra-node"
-        >
-          React + Node deployment on Heroku
-        </a></p>
-        <p><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a></p>
-      </header>
-    </div>
-  );
-
-}*/
-//export default App;
 
 const useScript = url => {
     useEffect(() => {
@@ -131,6 +68,7 @@ class App extends Component {
         });
 
     }*/
+    componentDidMount () {console.log("Token: "+this.state.token)}
     getHashParams() {
         var hashParams = {};
         var e, r = /([^&;=]+)=?([^&;]*)/g,
@@ -206,7 +144,6 @@ class App extends Component {
         return true;
     }
     render() {
-        var checkBox = document.getElementById("myCheck");
         return (
             <div class="app">
                 <div class="bg-image"><img src="/Albumcover.png"/></div>
