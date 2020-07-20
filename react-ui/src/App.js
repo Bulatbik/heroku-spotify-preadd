500d7b9b846735840a9b56d7528524c48e56c283
Bulatbik <bulatmegavolt@gmail.com>
2020-07-20 18:56:04 +0300

    test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
             .then(function (response) {
                 datares = response;
             }).catch(err => console.log(err));
-        console.log(datares.data);
+        console.dir(datares);
        // this.setState({albumName: datares.data.data.data.albumName})
         // console.log(datares.data);
         // console.dir(data);
