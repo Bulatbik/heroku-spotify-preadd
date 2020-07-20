49accb798af664cb4f2efff5dde6da0ef4849179
Bulatbik <bulatmegavolt@gmail.com>
2020-07-20 14:16:48 +0300

    test

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -4,12 +4,36 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import MusicProvider from './MusicProvider';
+import { Amplify } from 'aws-amplify';
+import config from './config';
 //ReactDOM.render(<App />, document.getElementById('root'));
 //var script = document.createElement("script");
 //script.src = 'https://js-cdn.music.apple.com/musickit/v1/musickit.js';
 //document.body.appendChild(script);
 //script.async = true;
-
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Storage: {
+        region: config.s3.REGION,
+        bucket: config.s3.BUCKET,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+        endpoints: [
+            {
+                name: "sites",
+                endpoint: config.apiGateway.URL,
+                region: config.apiGateway.REGION
+            },
+        ]
+    }
+});
 let musicProvider = MusicProvider.sharedProvider();
 musicProvider.configure();
 let musicInstance = musicProvider.getMusicInstance();
--- server/index.js
+++ server/index.js
@@ -32,7 +32,31 @@ global.Headers = fetch.Headers;
 const privateKey = fs.readFileSync(__dirname+"/AuthKey.p8").toString();
 const teamId     = "6UD2Y7J6SN";
 const keyId      = "6PAGB4SZ4L";
-
+import { Amplify } from 'aws-amplify';
+import config from './config';
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Storage: {
+        region: config.s3.REGION,
+        bucket: config.s3.BUCKET,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+        endpoints: [
+            {
+                name: "sites",
+                endpoint: config.apiGateway.URL,
+                region: config.apiGateway.REGION
+            },
+        ]
+    }
+});
 let rule = new schedule.RecurrenceRule();
 rule.tz = 'America/Chicago';
 // runs at 15:00:00
