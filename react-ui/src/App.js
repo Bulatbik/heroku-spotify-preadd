735573f662533d907dc6e44ae2138b3895c4b352
Bulatbik <bulatmegavolt@gmail.com>
2020-07-22 10:50:19 +0300

    case insensitivity

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
         console.log(sub);
 //sub is 'subdomain', 'domain', type is 'com'
         var datares;
-        let data =  await axios.post('https://young-peak-41948.herokuapp.com/createTheSite', {linkID:sub+"."+window.location.pathname.substring(1)})
+        let data =  await axios.post('https://young-peak-41948.herokuapp.com/createTheSite', {linkID:sub.toLowerCase()+"."+window.location.pathname.substring(1).toLowerCase()})
             .then(function (response) {
                 datares = response.data;
             }).catch(err => console.log(err));
