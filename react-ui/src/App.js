90839cb7038f4d040c9500cd2e08f6ad1777a54f
Adriel Lopez <adriel@endlessdigital.co>
2020-07-26 14:18:54 -0500

    removed artwork css + editable h3 test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -147,7 +147,7 @@ class App extends Component {
                 <div id="contentfadein" class="content-container">
                     <h1 class="h1">{this.state.artistName}</h1>
                     <h2 class="h2">{this.state.byTitle}</h2>
-                    <h3 class="h3">Available</h3>
+                    <h3 class="h3">{this.state.description}</h3>
                     <div>
                         <a class="buttonView" onClick={() => this.OnSubmitForm()}>
                             <img class="spotifyLogo" src="/Spotify_Logo_RGB_Green.png" />
