135d7d5360bcc58e63f6bf0c320c85c27ebb7f96
Bulatbik <bulatmegavolt@gmail.com>
2020-08-08 10:10:44 +0300

    fixes w loading screen

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -62,7 +62,8 @@ class App extends Component {
         console.dir(datares.data.albumName);
         var artwork = "https://music-dashboard-uploads.s3.us-east-2.amazonaws.com/private/"+datares.data.userId+"/"+datares.data.attachment;
         var byTitle = datares.data.albumName;
-       await this.setState({title:datares.data.albumName,artworkLink: artwork, artistName: datares.data.artistName, description: datares.data.description, byTitle: byTitle, UPC: datares.data.UPC}).then(this.setState({ done: true }));
+       await this.setState({title:datares.data.albumName,artworkLink: artwork, artistName: datares.data.artistName, description: datares.data.description, byTitle: byTitle, UPC: datares.data.UPC});
+        this.setState({ done: true })
        // this.setState({albumName: datares.data.data.data.albumName})
         // console.log(datares.data);
         // console.dir(data);
