9c18642febb20effe1125811a7b29ab4bfd7c059
Bulatbik <bulatmegavolt@gmail.com>
2020-08-17 16:13:04 +0300

    test§

--- server/index.js
+++ server/index.js
@@ -323,8 +323,8 @@ async function scheduler() {
             method: 'put',
             url: 'https://3n7l32gl97.execute-api.us-east-2.amazonaws.com/prod/clicks/'+albumId,
             headers: {
-                'X-Amz-Date': '20200817T120432Z',
-                'Authorization': 'AWS4-HMAC-SHA256 Credential=AKIAXDN6B3K7VJCHHJQP/20200817/us-east-2/execute-api/aws4_request, SignedHeaders=host;x-amz-date, Signature=d158d7856a29fa81d913a82c69b2f0c84752c70661d2f9aa295e28df82fe7a7c'
+                'X-Amz-Date': '20200817T131159Z',
+                'Authorization': 'AWS4-HMAC-SHA256 Credential=AKIAXDN6B3K7VJCHHJQP/20200817/us-east-2/execute-api/aws4_request, SignedHeaders=host;x-amz-date, Signature=681b794c381a6a76d36427a1cee905b2e72d60b69a388ccf5955fe8c7defe2e5'
             }
         };
         axios(config)
