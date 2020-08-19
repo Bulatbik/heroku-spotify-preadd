e8a4e89e3c244612c352f4fb3b26e9d416ba3ee0
Bulatbik <bulatmegavolt@gmail.com>
2020-08-19 13:39:34 +0300

    test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
        let location = await axios.get('https://api.hostip.info/get_json.php');
 //sub is 'subdomain', 'domain', type is 'com'
         var datares;
-        let data =  await axios.post('https://endlss.herokuapp.com/createTheSite', {linkID:sub.toLowerCase()+"."+window.location.pathname.substring(1, s.indexOf('/')).toLowerCase(), location:location.data.city+"-"+location.data.country_name})
+        let data =  await axios.post('https://endlss.herokuapp.com/createTheSite', {linkID:sub.toLowerCase()+"."+window.location.pathname.substring(1, window.location.pathname.indexOf('/')).toLowerCase(), location:location.data.city+"-"+location.data.country_name})
             .then(function (response) {
                 datares = response.data;
             }).catch(err => console.log(err));
