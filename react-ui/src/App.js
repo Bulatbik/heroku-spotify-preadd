b4883eb5247ad1ce07a0f3f8525cb4bd8f58c19f
Bulatbik <bulatmegavolt@gmail.com>
2020-08-22 17:17:50 +0300

    test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -206,19 +206,18 @@ class App extends Component {
                             <button class="buttonApple" id="apple-music-authorize-button">Pre-Add on Apple Music</button>
                         </a>
                             <div class="checkboxcolumn">
+                                {!this.state.openEmailModal ? (
                                 <label class="checkboxContainer">Get updates from {this.state.artistName}
                                     <input type="checkbox" id="myCheck" checked onClick="this.checked=!this.checked;"/>
                                     <span class="checkmark"></span>
                                 </label>
-                            </div>
-                            {this.state.openEmailModal && (
-                            <div class="checkboxcolumn">
+                                ) : (
                                 <label className="checkboxContainer">Confirm your email address below
-                                    </label>
                                 <input type="text" id="email" name="email"/>
                                 <input type="submit" value="Submit"/>
+                                </label>
+                                    )}
                             </div>
-                                )}
                         <div class="legalfooter">
                                 <div class="legaltext"><p>By using this service, you agree to our <a class="legallinks" href="https://terms.endlessdigital.co" target="_blank"><br />Terms of Service</a> &
                                 <a class="legallinks" href="https://privacy.endlessdigital.co" target="_blank"> Privacy Policy</a>.</p>
