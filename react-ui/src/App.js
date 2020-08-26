29e927cf37e0c3aca930e72295d476166e21ae35
Adriel Lopez <adriel@endlessdigital.co>
2020-08-26 15:46:54 -0500

    adjustments

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -223,7 +223,7 @@ class App extends Component {
                     <h2 class="h2">{this.state.byTitle}</h2>
                     <h3 class="h3">Available September 4</h3>
                     </div>
-                    <div style={{marginTop: '20%'}}>
+                    <div style={{marginTop: '23%'}}>
                         <div>
                         <a class="buttonView" onClick={() => this.OnSubmitForm()}>
                             <img class="spotifyLogo" src="/Spotify_Logo_RGB_Green.png" />
