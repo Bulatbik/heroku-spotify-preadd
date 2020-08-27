f43abaaaabb121ff590c11486a078a8971f67612
Adriel Lopez <adriel@endlessdigital.co>
2020-08-27 16:57:08 -0500

    changed spinner color

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -231,7 +231,7 @@ class App extends Component {
                 <div class="bg-image"><img style={this.state.done ? {} : {display: 'none'}} src={this.state.artworkLink} onLoad={() => this.setState({done: true})}/></div>
                 {!this.state.done ? (
                     <div class="loading-container">
-                    <ReactLoading type={"spin"} color={"white"} />
+                    <ReactLoading type={"spin"} color={"#FEB46D"} />
                     </div>
                 ) : (
                 <div id="contentfadein" class="content-container">
