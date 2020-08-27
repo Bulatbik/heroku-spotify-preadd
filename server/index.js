a2cefe6887e5eea40f3927e33ff0fdec9d4aa210
Adriel Lopez <adriel@endlessdigital.co>
2020-08-27 17:50:18 -0500

    changed instagram social codes

--- server/index.js
+++ server/index.js
@@ -653,7 +653,7 @@ async function clickCount(linkId, socialCode) {
         var socialCode = link.slice(-2);
         console.log(socialCode);
         var locdata;
-        if(socialCode==="IGB"||socialCode==="IGS"||socialCode==="TW"||socialCode==="FA"||socialCode==="YD"||socialCode==="WE"||socialCode==="IA"||socialCode==="FA"||socialCode==="SN"||socialCode==="TT"||socialCode==="CM"){
+        if(socialCode==="IB"||socialCode==="IS"||socialCode==="TW"||socialCode==="FA"||socialCode==="YD"||socialCode==="WE"||socialCode==="IA"||socialCode==="FA"||socialCode==="SN"||socialCode==="TT"||socialCode==="CM"){
             console.log("THERE is A code");
             link = link.slice(0, -2).toLowerCase();
              locdata = JSON.stringify({
