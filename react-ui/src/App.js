497314e90190e32d5d87b0cd1136a162f61b9db9
Adriel Lopez <adriel@endlessdigital.co>
2020-09-10 07:32:19 -0500

    adjustments

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -306,7 +306,7 @@ class App extends Component {
                                 <div class="legaltextbasic basic"><p>By using this service, you agree to our <a class="legallinks basic" href="https://terms.endlessdigital.co" target="_blank"><br />Terms of Service</a> &
                                 <a class="legallinks basic" href="https://privacy.endlessdigital.co" target="_blank"> Privacy Policy</a>.</p>
                                 </div>
-                        <div class="poweredby"><p class="poweredbytext">powered by</p><a href="https://instagram.com/endlessdigital"><img class="endlesslogobasic" src="/endlesslogosega.png"/></a></div>
+                        <div class="poweredby">powered by<a href="https://instagram.com/endlessdigital"><img class="endlesslogobasic" src="/endlesslogosega.png"/></a></div>
                         </div>
                     </div>
                     )}
