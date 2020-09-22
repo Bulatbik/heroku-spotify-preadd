00fda88ed635cab21860ff8af274bc116c1ad92e
Adriel Lopez <adriel@endlessdigital.co>
2020-09-22 01:39:20 -0500

    test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -462,6 +462,16 @@ class App extends Component {
                                     <span class="checkmark"></span>
                                 </label>
                             </div>
+<<<<<<< HEAD
+=======
+                            {!this.state.isInstagramBrowser ? (  //Right now im showing the notice from any browser for dbg purposes. Remove the "!" to make it work
+                              <a class="buttonViewApple3" onClick={() => this.OpenAppleInstNotice()} >
+                                <img class="appleLogo" src="/Apple_Music_Icon_RGB_Black.svg" />
+                                <button class="buttonApple">Pre-Add on Apple Music
+                                </button>
+                              </a>
+                                ):(
+>>>>>>> parent of 4a6f4ba... abort
                                 <a className="buttonViewApple3" onClick={() => this.signIn()} id="apple-music-authorize">
                                 <img className="appleLogo" src="/Apple_Music_Icon_RGB_Black.svg"/>
                                 <button className="buttonApple" id="apple-music-authorize-button">Pre-Add on Apple Music
