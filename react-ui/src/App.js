bf480c9917e460a7df755c00f959551dd005ef15
Bulatbik <bulatmegavolt@gmail.com>
2020-09-28 18:57:51 -0400

    test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -245,7 +245,7 @@ class App extends Component {
         //  var checkBox = document.getElementById("myCheck");
         //  checkBox.checked = !this.checkedSpot;
         this.setState({checkedSpot: !this.state.checkedSpot})
-        var testContainer = document.getElementById('checkmark');
+        var testContainer = document.getElementById('insidecheckS');
         if(this.state.checkedSpot===true) {
             testContainer.style.borderLeft = "2px solid red";
             testContainer.style.borderBottom = "2px solid red";
