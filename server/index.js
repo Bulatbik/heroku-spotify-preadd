c933f096c192dd625084065226fc2572a930dc13
Bulatbik <bulatmegavolt@gmail.com>
2020-09-30 03:33:31 -0400

    test

--- server/index.js
+++ server/index.js
@@ -199,7 +199,7 @@ async function scheduler() {
                     uniqueNotReleasedUPDS.push(response.data[i].albumUPC);
                 }else{
                     console.log("____________________________________");
-                    console.log(err.request);
+                    console.log(err.data);
                     console.log("____________________________________");
                 }
             }
