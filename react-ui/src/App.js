b4a5fcd33bfcc518516c90ef3e41ff2eaaf9451a
Adriel Lopez <adriel@endlessdigital.co>
2020-10-02 00:53:16 -0500

    test

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -349,6 +349,7 @@ class App extends Component {
                 <div class="bg-image"><img style={this.state.done ? {} : {display: 'none'}} src={this.state.artworkLink} onLoad={() => this.setState({done: true})}/></div>
                 {!this.state.done ? (
                     <div class="loading-container">
+                    <ReactLoading type={"spin"} color={"#ffffff"} />
                     </div>
                 ) : (
                     <div style={{height:"100%"}}>
